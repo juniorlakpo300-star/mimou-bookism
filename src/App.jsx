30b96f0af5a875731b1620987b1214e7f0981846
juniorlakpo300-star <juniorlakpo300@gmail.com>
2026-09-02 22:21:50 +0000

    Add global Supabase auth provider

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,23 @@ import Publier from './pages/Publier'
 import Reader from './pages/Reader'
 import Connexion from './pages/Connexion'
 import Inscription from './pages/Inscription'
+import { AuthProvider } from './AuthContext.jsx'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Catalogue />} />
-        <Route path="/catalogue" element={<Catalogue />} />
-        <Route path="/publier" element={<Publier />} />
-        <Route path="/read/:id" element={<Reader />} />
-        <Route path="/lire/:id" element={<Reader />} />
-        <Route path="/connexion" element={<Connexion />} />
-        <Route path="/inscription" element={<Inscription />} />
-        <Route path="*" element={<div className="state"><h1>404 - Page non trouvée</h1></div>} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Catalogue />} />
+          <Route path="/catalogue" element={<Catalogue />} />
+          <Route path="/publier" element={<Publier />} />
+          <Route path="/read/:id" element={<Reader />} />
+          <Route path="/lire/:id" element={<Reader />} />
+          <Route path="/connexion" element={<Connexion />} />
+          <Route path="/inscription" element={<Inscription />} />
+          <Route path="*" element={<div className="state"><h1>404 - Page non trouvée</h1></div>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
