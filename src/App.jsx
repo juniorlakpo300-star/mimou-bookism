cd5014a850b7ff74e0ea3701f3c1f5e8db791533
juniorlakpo300-star <juniorlakpo300@gmail.com>
2026-09-03 21:00:14 +0000

    feat: add favorites route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Choix from './pages/Choix'
 import Catalogue from './pages/Catalogue'
 import Manga from './pages/Manga'
+import Favoris from './pages/Favoris'
 import Publier from './pages/Publier'
 import Reader from './pages/Reader'
 import Ecrivain from './pages/Ecrivain'
@@ -22,6 +23,7 @@ export default function App() {
           <Route path="/livres" element={<Catalogue />} />
           <Route path="/catalogue" element={<Catalogue />} />
           <Route path="/mangas" element={<Manga />} />
+          <Route path="/favoris" element={<Favoris />} />
           <Route path="/publier" element={<Publier />} />
           <Route path="/ecrivain" element={<Ecrivain />} />
           <Route path="/admin" element={<Admin />} />
