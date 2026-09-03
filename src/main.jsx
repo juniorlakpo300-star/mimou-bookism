240ab0a0481d3a7c478d07a4f9a5d241065cf06f
juniorlakpo300-star <juniorlakpo300@gmail.com>
2026-09-03 22:46:48 +0000

    fix: appliquer le design recherche livres mangas globalement

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './theme.css'
 import './features.css'
+import './dictionary.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
